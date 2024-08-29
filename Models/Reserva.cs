ad660fbbc4fdd69f9ea1516cf0daebb86e616c31
viniciusfrancodeoliveira <viniciusfranco56@gmail.com>
2024-08-29 00:29:22 -0300

    Solução do desafio de código - adição funçõesclasse Reserva

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,15 +17,15 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
-            {
-                Hospedes = hospedes;
-            }
-            else
-            {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
-            }
+            
+                if (hospedes.Count <= Suite.Capacidade)
+                    {
+                        Hospedes = hospedes;
+                    }
+                    else
+                    {
+                        throw new SystemException("A quantidade de hóspedes não pode exceder a capacidade da suíte");
+                    }
         }
 
         public void CadastrarSuite(Suite suite)
@@ -35,25 +35,21 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            // Retorna a quantidade de hóspedes 
+            int quantidadeDeHospedes = Hospedes.Count;
+            return quantidadeDeHospedes;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
             decimal valor = 0;
+            valor = DiasReservados * Suite.ValorDiaria;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+           //Aplica 10% de Desconto se DiasReservados >= 10
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor -= (valor /10);
             }
-
             return valor;
         }
     }
