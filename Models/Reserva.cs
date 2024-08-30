6885e90812be54091214754e38eb16013e26a459
viniciusfrancodeoliveira <viniciusfranco56@gmail.com>
2024-08-30 20:53:48 -0300

    Correção TODO- Reserva.cs

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,8 +15,7 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
+            // Verifica se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             
                 if (hospedes.Count <= Suite.Capacidade)
                     {
